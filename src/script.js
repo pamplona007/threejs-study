ad73dbf439105ca08d4ff7187776b7418a2a9f87
Lucas Pamplona <lucas250598@gmail.com>
2022-11-18 22:44:20 -0300

    Lesson 2 - Animation principles

--- src/script.js
+++ src/script.js
@@ -1,27 +1,43 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import gsap from 'gsap';
+import { AxesHelper, BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import './style.scss';
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
 
 const scene = new Scene();
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const mesh = new Mesh(geometry, material);
-
-scene.add(mesh);
-
-const sizes = {
-    width: 800,
-    height: 600,
-};
-
 const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+
+const box = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0xff0000 }),
+);
+
+scene.add(box);
 scene.add(camera);
 
 const canvas = document.querySelector('.webgl');
 const renderer = new WebGLRenderer({
-    canvas: canvas,
+    canvas,
 });
+
+const axesHelper = new AxesHelper();
+scene.add(axesHelper);
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+gsap.to(box.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(box.position, { duration: 1, delay: 2, x: 0 });
+
+const animate = () => {
+    camera.lookAt(box.position);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+animate();
